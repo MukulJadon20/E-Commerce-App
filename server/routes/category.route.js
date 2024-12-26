a58f782b49bec89d96487ce62b084954989336f1
Mukul Jadon <147290552+MukulJadon20@users.noreply.github.com>
2024-12-26 12:06:34 +0530

    final commit

--- server/routes/category.route.js
+++ server/routes/category.route.js
@@ -1,17 +1,12 @@
-import { Router } from "express";
-import auth from "../middleware/auth.js";
-import {
-  CashOnDeliveryOrderController,
-  getOrderDetailsController,
-  paymentController,
-  webhookStripe,
-} from "../controllers/order.controller.js";
+import { Router } from 'express'
+import auth from '../middleware/auth.js'
+import { AddCategoryController, deleteCategoryController, getCategoryController, updateCategoryController } from '../controllers/category.controller.js'
 
-const orderRouter = Router();
+const categoryRouter = Router()
 
-orderRouter.post("/cash-on-delivery", auth, CashOnDeliveryOrderController);
-orderRouter.post("/checkout", auth, paymentController);
-orderRouter.post("/webhook", webhookStripe);
-orderRouter.get("/order-list", auth, getOrderDetailsController);
+categoryRouter.post("/add-category",auth,AddCategoryController)
+categoryRouter.get('/get',getCategoryController)
+categoryRouter.put('/update',auth,updateCategoryController)
+categoryRouter.delete("/delete",auth,deleteCategoryController)
 
-export default orderRouter;
+export default categoryRouter
