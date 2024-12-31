dac48b79a8a92ecdbc6e4fa9cae2c738db16299f
Mukul Jadon <147290552+MukulJadon20@users.noreply.github.com>
2024-12-31 14:33:15 +0530

    commit

--- client/src/commom/SummaryApi.js
+++ client/src/commom/SummaryApi.js
@@ -1,4 +1,4 @@
-export const baseURL = "http://localhost:8080"
+export const baseURL = "https://e-commerce-app-4-u5bg.onrender.com"
 // export const baseURL = import.meta.env.VITE_API_URL
 
 const SummaryApi = {
